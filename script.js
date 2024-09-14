7c400cff6b66c1c20304d890adb193ae5b9500a8
bibi <brendagovha1@gmail.com>
2024-09-14 21:12:10 +0200

    added API to the Javascript code

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "2abt3d4e02aa5e6116ef006dbocad3a0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  console.log(apiUrl);
+}
+
 function searchFunction(event) {
   event.preventDefault();
   let formInput = document.querySelector("#search-form");
   let city = document.querySelector("#city-name");
   city.innerHTML = formInput.value;
+  searchCity(formInput.value);
 }
 
 let searchForm = document.querySelector("#search-engine");
