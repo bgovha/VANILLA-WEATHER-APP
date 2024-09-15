cb2b450b6837d60010b7207e848bee2ed8fb49d7
bibi <brendagovha1@gmail.com>
2024-09-15 22:39:27 +0200

    added the search functionaity using the API

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let city = document.querySelector("#city-name");
+
+  city.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
   let apiKey = "2abt3d4e02aa5e6116ef006dbocad3a0";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function searchFunction(event) {
   event.preventDefault();
   let formInput = document.querySelector("#search-form");
-  let city = document.querySelector("#city-name");
-  city.innerHTML = formInput.value;
+
   searchCity(formInput.value);
 }
 
 let searchForm = document.querySelector("#search-engine");
 searchForm.addEventListener("submit", searchFunction);
+
+searchCity("Harare");
