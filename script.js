d451efbc98bdcc2a589c203163410551834a0e71
bibi <brendagovha1@gmail.com>
2024-09-16 00:25:02 +0200

    injected more weather data

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let city = document.querySelector("#city-name");
+  let skyCoverage = document.querySelector("#sky-coverage");
+  let humidity = document.querySelector("#weather-description-value");
+  let windSpeed = document.querySelector("#wind-speed");
+  let dateElement = document.querySelector("#date");
+  let date = new Date(response.data.time * 1000);
 
   city.innerHTML = response.data.city;
 
-  temperatureElement.innerHTML = Math.round(temperature);
+  skyCoverage.innerHTML = response.data.condition.description;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  temperatureElement.innerHTML = `${Math.round(temperature)}°C`;
+  windSpeed.innerHTML = `${response.data.wind.speed}%`;
+  dateElement.innerHTML = setDate(date);
 }
 
 function searchCity(city) {
@@ -13,7 +22,25 @@ function searchCity(city) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
+function setDate(date) {
+  let minutes = date.getMinutes();
+  let hour = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hour}:${minutes}HRS`;
 
+  if (minutes < 10) {
+    minutes = `0 ${minutes}`;
+  }
+}
 function searchFunction(event) {
   event.preventDefault();
   let formInput = document.querySelector("#search-form");
